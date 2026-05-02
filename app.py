fe5eec0e4befb1ddd0faf80dec160bdf95901c92
deysocalcutta84-spec <deyso.calcutta84@gmail.com>
2026-05-02 16:46:55 +0530

    test3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Docker on AWS! Cloudnet calcutta dumdum Welcome All 2026 WB"
+    return "Hello from Docker on AWS! Cloudnet calcutta dumdum Welcome All 2026 West Bengal"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
